520d54d5c109d0e15a01ba1c0b1e44c1859cd60d
UWEGHREPHA EJIRO THANKGOD <ejineroz@pop-os.localdomain>
2022-07-07 09:30:08 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dropzone';
+  incomingItems = ['Carrots', 'Tomatoes', 'Onions', 'Apples', 'Avocados'];
+
+  availableItems = ['Oranges', 'Bananas', 'Cucumbers', 'Try to move me'];
+
+  soldItems = ['Pears', 'Strawberries', 'Limes', 'Mangoes'];
+
+  items = [
+    {value: 'Oranges', disabled: false},
+    {value: 'Bananas', disabled: true},
+    {value: 'Mangoes', disabled: false},
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
 }
+
+
+// For a Disabled Drag and Drop
+
+// import {Component} from '@angular/core';
+// import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   items = [
+//     {value: 'Oranges', disabled: false},
+//     {value: 'Bananas', disabled: true},
+//     {value: 'Mangoes', disabled: false},
+//   ];
+
+//   drop(event: CdkDragDrop<string[]>) {
+//     moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+//   }
+// }
